2989c9e1c8901408abcba812e3f3913b2001bb88
Rumen Roger <rumenroger@gmail.com>
2020-05-13 22:32:27 +0100

    B8 hash item replacement

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -82,5 +82,6 @@ p lottery_numbers.select { |number| number.even? } # second approach in 1 line
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 p users["Erik"][:lottery_numbers].push(7)
 # 8. Change Erik's hometown to Edinburgh
+p users["Erik"][:home_town] = "Edinburgh"
 # 9. Add a pet dog to Erik called "Fluffy"
 # 10. Add another person to the users hash
