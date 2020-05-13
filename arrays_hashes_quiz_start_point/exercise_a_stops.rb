697a692bbea507a938d91e53391b2c9423c2dcd0
Rumen Roger <rumenroger@gmail.com>
2020-05-13 19:51:18 +0100

    A.3 insert(index, obj)

--- arrays_hashes_quiz_start_point/exercise_a_stops.rb
+++ arrays_hashes_quiz_start_point/exercise_a_stops.rb
@@ -5,6 +5,7 @@ stops.push("Edinburgh Waverley")
 #2. Add "Glasgow Queen St" to the start of the array
 stops.unshift("Glasgow Queen St")
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+stops.insert(4, "Polmont")
 #4. Print out the index position of "Linlithgow"
 #5. Remove "Livingston" from the array using its name
 #6. Delete "Cumbernauld" from the array by index
