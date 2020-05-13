a9706a33741ddf55fa5268abbcc9c819775ea2b3
Rumen Roger <rumenroger@gmail.com>
2020-05-13 20:00:00 +0100

    A.7 .count

--- arrays_hashes_quiz_start_point/exercise_a_stops.rb
+++ arrays_hashes_quiz_start_point/exercise_a_stops.rb
@@ -7,12 +7,13 @@ stops.unshift("Glasgow Queen St")
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
 stops.insert(4, "Polmont")
 #4. Print out the index position of "Linlithgow"
-p stops.index("Linlithgow")
+p "Linlithgow is the stop number #{stops.index("Linlithgow")}"
 #5. Remove "Livingston" from the array using its name
 stops.delete("Livingston")
 #6. Delete "Cumbernauld" from the array by index
 stops.delete_at(2)
 #7. Print the number of stops there are in the array?
+p "There are #{stops.count} stops"
 #8. Show as many ways as you can to return "Falkirk High" from the array?
 #9. Reverse the positions of the stops in the array
 #10 Print out all the stops using a for loop
